4c4e19c0276f006fa694cfe2cac5c64721a7089b
Nursultan <65071183+Nursultan-coder@users.noreply.github.com>
2021-02-01 23:12:59 +0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+      n *= -1;
+  }
+  let str = String(n);
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+      newStr += str[i];
+  }
+  let num = Number(newStr);
+  return newStr;
 }
